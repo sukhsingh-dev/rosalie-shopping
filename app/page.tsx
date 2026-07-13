9a24729229893e5d04a0b915b19023348608e75f
Sukh Singh <devsukhjindersingh@gmail.com>
2026-07-13 21:38:03 +0530

    feat: initialize project structure with core components, assets, and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
-      <h1>Rosalie Shopping</h1>
+      <div className="video-overlay relative">
+        <video className="w-full object-cover aspect-[1/1.4] -mt-(--header-height) sm:aspect-16/7 sm:object-top" muted autoPlay loop preload="auto">
+          <source src="/desk-vid.mp4" type="video/mp4" media="(min-width: 640px)" />
+          <source src="/mob-vid.mp4" type="video/mp4" />
+        </video>
+        <div className="video-gradient z-1 absolute top-0 left-0 right-0 bottom-0" />
+      </div>
     </main>
   );
 }
